c679edf1fbb20500135ef04ee29e9524e18d4193
Norman Rasmussen <norman@rasmussen.co.za>
2024-08-31 15:55:02 -0700

    Query server for installed workshop items

--- ModStarter.cs
+++ ModStarter.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 using Timberborn.ModManagerScene;
 using Timberborn.Modding;
 using Timberborn.SerializationSystem;
+using Timberborn.SteamStoreSystem;
 
 namespace Mods.SteamInfo {
   internal class ModStarter : IModStarter {
@@ -20,7 +21,7 @@ namespace Mods.SteamInfo {
         Debug.Log("SteamInfo.ModPath = " + modEnvironment.ModPath);
       }
 
-      Debug.Log("Steam mods:");
+      Debug.Log("Steam mods (local cache):");
       var merger = new JsonMerger();
       var obrw = new ObjectSaveReaderWriter(merger);
       var modLoader = new ModLoader(obrw);
@@ -28,6 +29,84 @@ namespace Mods.SteamInfo {
         tuple.Deconstruct(out var state, out var mod);
         Debug.Log("- " + state.ToString().Replace("k_EItemState", "") + ": " + mod.ModDirectory.Directory.Name + "/" + mod.Manifest.Name + " (" + mod.Manifest.Version.AsFormattedString() + ")");
       }
+
+      Debug.Log("Triggering query for installed workshop items");
+      QueryInstalledWorkshopItems((items, param, ioFailure) => {
+        Debug.Log("Steam mods (subscribed at server):");
+        if (ioFailure) {
+          Debug.Log("- ioFailure");
+        };
+        if (items == null) {
+          Debug.Log("- null items");
+        };
+        foreach (var item in items) {
+          Debug.Log("- " + item.Title + "/" + item.Description + ": Created=" + item.UgcDetails.m_rtimeCreated + ", Updated=" + item.UgcDetails.m_rtimeUpdated);
+        };
+      });
+    }
+
+    // From https://gist.github.com/GMMan/d9305ffde52372d926662522ed9259b1
+
+    class SteamWorkshopItem {
+      public PublishedFileId_t FileId { get; set; }
+      public string Title { get; set; }
+      public string Description { get; set; }
+      public string Metadata { get; set; }
+      public ERemoteStoragePublishedFileVisibility Visibility { get; set; }
+      public List<string> Tags { get; set; }
+      public Dictionary<string, string> KeyValues { get; set; }
+      public string UpdateLanguage { get; set; }
+      public string UpdateContentPath { get; set; }
+      public string UpdatePreviewPath { get; set; }
+      public string UpdateChangeNotes { get; set; }
+      public SteamUGCDetails_t UgcDetails { get; internal set; }
+
+      public SteamWorkshopItem() {
+        Tags = new List<string>();
+        KeyValues = new Dictionary<string, string>();
+      }
+    }
+
+    List<CallResult<SteamUGCQueryCompleted_t>> ugcQueryCompletedCallResults = new List<CallResult<SteamUGCQueryCompleted_t>>();
+    delegate void ugcQueryCompletedHandler(List<SteamWorkshopItem> items, SteamUGCQueryCompleted_t param, bool ioFailure);
+
+    bool QueryInstalledWorkshopItems(ugcQueryCompletedHandler handler) {
+      uint subscribedCount = SteamUGC.GetNumSubscribedItems();
+      PublishedFileId_t[] subscribedFiles = new PublishedFileId_t[subscribedCount];
+      if (SteamUGC.GetSubscribedItems(subscribedFiles, (uint)subscribedFiles.Length) != subscribedCount)
+        return false;
+
+      UGCQueryHandle_t query = SteamUGC.CreateQueryUGCDetailsRequest(subscribedFiles, (uint)subscribedFiles.Length);
+      SteamUGC.SetReturnLongDescription(query, true);
+      SteamUGC.SetReturnMetadata(query, true);
+      SteamAPICall_t apiCall = SteamUGC.SendQueryUGCRequest(query);
+      var callResult = CallResult<SteamUGCQueryCompleted_t>.Create();
+      callResult.Set(apiCall, (param, ioFailure) =>
+      {
+        List<SteamWorkshopItem> items = null;
+        if (!ioFailure && param.m_eResult == EResult.k_EResultOK) {
+          items = new List<SteamWorkshopItem>();
+          for (uint i = 0; i < param.m_unNumResultsReturned; ++i) {
+            SteamWorkshopItem item = new SteamWorkshopItem();
+            SteamUGC.GetQueryUGCResult(query, i, out SteamUGCDetails_t details);
+            item.FileId = details.m_nPublishedFileId;
+            item.Title = details.m_rgchTitle;
+            item.Description = details.m_rgchDescription;
+            SteamUGC.GetQueryUGCMetadata(query, i, out string metadata, Constants.k_cchDeveloperMetadataMax);
+            item.Metadata = metadata;
+            item.Visibility = details.m_eVisibility;
+            item.Tags.AddRange(details.m_rgchTags.Split(','));
+            // KeyValues should be converted to a List<KeyValuePair<string, string>>, so ignore for now
+            item.UgcDetails = details;
+            items.Add(item);
+          }
+        }
+        ugcQueryCompletedCallResults.Remove(callResult);
+        SteamUGC.ReleaseQueryUGCRequest(query);
+        handler(items, param, ioFailure);
+      });
+      ugcQueryCompletedCallResults.Add(callResult);
+      return true;
     }
 
     // From Timberborn.Modding.ModRepository
